6b7cf2a8735c5652a4c018c6fb1c1798ca4f85fe
joey@emakefun.com <joey@emakefun.com>
2020-05-16 17:39:16 +0800

    测试

--- main.ts
+++ main.ts
@@ -1,155 +1,57 @@
-/**
- * makecode Four Digit Display (TM1650) Package.
- * From microbit/micropython Chinese community.
- * http://www.micropython.org.cn
- */
+/*
+TTP229 block
+*/
+  
+//% weight=8 color=#FFD700  block="TTP229"
+namespace ttp229
+{
 
-/**
- * TM1650 digit Display
- */
-//% weight=100 color=#64C800  
-//%block="TM1650"
-namespace TM1650{
+     //% blockId=ttp229_GetKeyCode
+     //% block="touch_keyboard|SDOPIN %SDO|SCLPIN %SCL"
+     export function GetKeyCode(SDO: DigitalPin, SCL: DigitalPin): string
+     {
+ 
+          let DATA = 0;
 
-    let COMMAND_I2C_ADDRESS = 0x24
-    let DISPLAY_I2C_ADDRESS = 0x34
-    let _SEG = [0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71];
+          pins.digitalWritePin(SDO, 1);
+          control.waitMicros(93);
 
-    let _intensity = 3
-    let dbuf = [0, 0, 0, 0]
+          pins.digitalWritePin(SDO, 0);
+          control.waitMicros(10);
 
-    /**
-     * send command to display
-     * @param is command, eg: 0
-     */
-    function cmd(c: number) {
-        pins.i2cWriteNumber(COMMAND_I2C_ADDRESS, c, NumberFormat.Int8BE)
-    }
+          for (let i = 0; i < 16; i++)
+          {
 
-    /**
-     * send data to display
-     * @param is data, eg: 0
-     */
-    function dat(bit: number, d: number) {
-        pins.i2cWriteNumber(DISPLAY_I2C_ADDRESS + (bit % 4), d, NumberFormat.Int8BE)
-    }
+               pins.digitalWritePin(SCL, 1);
+               pins.digitalWritePin(SCL, 0);
 
-    /**
-     * turn on display
-     */
-    //% blockId="TM1650_ON" 
-    //%block="turn on display"
-    //% weight=50 blockGap=8
-    export function On() {
-        cmd(_intensity * 16 + 1)
-    }
+               DATA |= pins.digitalReadPin(SDO) << i;   
+          }
 
-    /**
-     * turn off display
-     */
-    //% blockId="TM1650_OFF" block="turn off display"
-    //% weight=50 blockGap=8
-    export function Off() {
-        _intensity = 0
-        cmd(0)
-    }
+          control.waitMicros(2 * 1000);
 
-    /**
-     * clear display content
-     */
-    //% blockId="TM1650_CLEAR" 
-    //%block="clear display"
-    //% weight=40 blockGap=8
-    export function Clear() {
-        dat(0, 0)
-        dat(1, 0)
-        dat(2, 0)
-        dat(3, 0)
-        dbuf = [0, 0, 0, 0]
-    }
+          switch (DATA&0xFFFF)
+          {     
+               case 0xFFFE: return "1"; 
+               case 0xFFFD: return "2"; 
+               case 0xFFFB: return "3"; 
+               case 0xFFEF: return "4";  
+               case 0xFFDF: return "5"; 
+               case 0xFFBF: return "6"; 
+               case 0xFEFF: return "7"; 
+               case 0xFDFF: return "8"; 
+               case 0xFBFF: return "9"; 
+               case 0xDFFF: return "0"; 
+               case 0xFFF7: return "A"; 
+               case 0xFF7F: return "B"; 
+               case 0xF7FF: return "C"; 
+               case 0x7FFF: return "D"; 
+               case 0xEFFF: return "*"; 
+               case 0xBFFF: return "#";      
 
-    /**
-     * show a digital in given position
-     * @param digit is number (0-15) will be shown, eg: 1
-     * @param bit is position, eg: 0
-     */
-    //% blockId="TM1650_DIGIT" 
-    //%block="show digit %num|at %bit" advanced=true
-    //% weight=80 blockGap=8
-    //% num.max=15 num.min=0
-    export function Digit(num: number, bit: number) {
-        dbuf[bit % 4] = _SEG[num % 16]
-        dat(bit, _SEG[num % 16])
-    }
-
-    /**
-     * show a number in display
-     * @param num is number will be shown, eg: 100
-     */
-    //% blockId="TM1650_SHOW_NUMBER" 
-    //%block="show number %num" 
-    //% weight=100 blockGap=8
-    export function ShowNumber(num: number) {
-        if (num < 0) {
-            dat(0, 0x40) // '-'
-            num = -num
-        }
-        else
-            digit(Math.idiv(num, 1000) % 10, 0)
-        digit(num % 10, 3)
-        digit(Math.idiv(num, 10) % 10, 2)
-        digit(Math.idiv(num, 100) % 10, 1)
-    }
-
-    /**
-     * show a number in hex format
-     * @param num is number will be shown, eg: 123
-     */
-    //% blockId="TM1650_SHOW_HEX_NUMBER" 
-    //%block="show hex number %num" advanced=true
-    //% weight=90 blockGap=8
-    export function ShowHex(num: number) {
-        if (num < 0) {
-            dat(0, 0x40) // '-'
-            num = -num
-        }
-        else
-            digit((num >> 12) % 16, 0)
-        digit(num % 16, 3)
-        digit((num >> 4) % 16, 2)
-        digit((num >> 8) % 16, 1)
-    }
-
-    /**
-     * show Dot Point in given position
-     * @param bit is positiion, eg: 0
-     * @param show is true/false, eg: true
-     */
-    //% blockId="TM1650_SHOW_DP" 
-    //%block="show dot point %bit|show %num" advanced=true
-    //% weight=80 blockGap=8
-    export function ShowDpAt(bit: number, show: boolean) {
-        if (show) dat(bit, dbuf[bit % 4] | 0x80)
-        else dat(bit, dbuf[bit % 4] & 0x7F)
-    }
-
-    /**
-     * set display intensity
-     * @param dat is intensity of the display, eg: 3
-     */
-    //% blockId="TM1650_INTENSITY" 
-    //%block="set intensity %dat" 
-    //% weight=70 blockGap=8
-    export function SetIntensity(dat: number) {
-        if ((dat < 0) || (dat > 8))
-            return;
-        if (dat == 0)
-            off()
-        else {
-            _intensity = dat
-            cmd((dat << 4) | 0x01)
-        }
-    }
-
-    on();
+               default: return " ";
+          }          
+     
+     }
 }
+
