1dacea185d68ca60bc1996230579414afe10d8bb
joey@emakefun.com <joey@emakefun.com>
2020-05-16 16:53:55 +0800

    2

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@
 /**
  * TM1650 digit Display
  */
-//% weight=100 color=#64C800  block="TM1650"
+//% weight=100 color=#64C800 icon="1" block="TM1650"
 namespace TM1650{
 
     let COMMAND_I2C_ADDRESS = 0x24
